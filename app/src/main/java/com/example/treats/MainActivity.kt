188e2410866b1dc0ee605fd850742f7ddad949ae
Dennis Kimani <denoh6804@gmail.com>
2021-01-26 18:19:04 +0300

    BottomNavBar setup with the Nav controller to provide the navigation to Bottom Navigation Bar.

--- app/src/main/java/com/example/treats/MainActivity.kt
+++ app/src/main/java/com/example/treats/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.treats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Setup navigation on the Bottom Nav Bar
+        // Initialize the Bottom Nav Bar using the findViewById
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        //Initialize Nav controller using the findNavController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Merge the controller and Bottom navbar view with setupWithController method
+        bottomNavBar.setupWithNavController(navController)
+
     }
 }
